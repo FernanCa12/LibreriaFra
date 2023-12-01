bbd5694b530b33b3a635032efc1153536de0e799
Fernando Cardozo <cardozof123@outlook.com>
2023-12-01 00:53:24 -0500

    modificaciones

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import modelo.dao.ClienteDao;
+import modelo.modelos.Cliente;
+
 public class Main {
     public static void main(String[] args) {
+        ClienteDao cliente = new Cliente();
 
+        cliente.getAll().forEach(System.out::println);
     }
 }
